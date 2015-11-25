d4d1b96ad678ac7874909808e5fb54e58791a859
John Törnblom <john.tornblom@gmail.com>
2015-11-25 19:07:26 +0100

    test: added some checks for scoping rules on variable access

---
+++ tests/test_scope_rules.py
@@ -0,0 +1,40 @@
+# encoding: utf-8
+# Copyright (C) 2015 John Törnblom
+
+from utils import RSLTestCase
+from utils import evaluate
+
+import rsl
+
+
+class TestScopeRules(RSLTestCase):
+
+    @evaluate
+    def test_access_global_from_function(self, rc):
+        '''
+        .assign x = 5
+        .function f
+            .exit x
+        .end function
+        .invoke f()
+        '''
+        self.assertIsInstance(rc, rsl.symtab.SymtabException)
+        
+    @evaluate
+    def test_recursive_function(self, rc):
+        '''
+        .function f
+          .param integer x
+          .assign y = x
+          .if (x > 0)
+            .invoke res = f(x - 1)
+          .end if
+          .assign attr_value = y
+        .end function
+        
+        .invoke res = f(3)
+        
+        .exit res.value
+        '''
+        self.assertEqual(rc, 3)
+        
