dfb5bb07d717d4a8ffbe5bf0c6e3a7e60920282e
John Törnblom <john.tornblom@gmail.com>
2016-09-03 13:01:28 +0200

    version bump

--- rsl/version.py
+++ rsl/version.py
@@ -1,12 +1,12 @@
 # encoding: utf-8
-# Copyright (C) 2015 John Törnblom
+# Copyright (C) 2016 John Törnblom
 '''
 Keep version info in one single place
 '''
 
 
 name = 'pyrsl'
-date = '2016-06-20'
-release = '0.5.1'
+date = '2016-09-03'
+release = '0.6.0'
 
 complete_string = '%s v%s (%s)' % (name, release, date)
