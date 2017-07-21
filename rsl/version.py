9bec2e6db743f1fe5528316035d2de6505867003
John Törnblom <john.tornblom@gmail.com>
2017-07-22 01:28:29 +0200

    bump version to 1.0.0

--- rsl/version.py
+++ rsl/version.py
@@ -4,9 +4,10 @@
 Keep version info in one single place
 '''
 
-
 name = 'pyrsl'
-date = '2016-09-03'
-release = '0.6.0'
+date = '2017-07-22'
+version = '1.0'
+release = '1.0.0' #  # ensure that this is the same as in setup.py
 
 complete_string = '%s v%s (%s)' % (name, release, date)
+
